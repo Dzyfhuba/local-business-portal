8268ccc6ccea3615ff5fed48a03ab1d57bd212fe
Dzyfhuba <hafidz21ub@gmail.com>
2022-07-28 15:14:49 +0700

    add supabase success

--- api/database/factories/PostFactory.ts
+++ api/database/factories/PostFactory.ts
@@ -3,7 +3,7 @@ import Factory from '@ioc:Adonis/Lucid/Factory'
 
 export default Factory.define(Post, ({ faker }) => {
   return {
-    user_id: Math.random() * 10 + 1,
+    user_id: Math.floor(Math.random() * 12 + 1),
     title: faker.lorem.words(),
     images: `${faker.image.imageUrl()};${faker.image.imageUrl()};${faker.image.imageUrl()};${faker.image.imageUrl()}`,
     content: faker.lorem.paragraphs(10),
--- client/src/Pages/Control/Stall/Create.jsx
+++ client/src/Pages/Control/Stall/Create.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import { supabase } from '../../../Config/SupabaseClient'
 
 const Create = props => {
-  return (
-    <div>Create</div>
-  )
+    const [image, setImage] = useState(String)
+
+    useEffect(() => {
+        getImage().then(data => setImage(data))
+    }, [])
+
+    const getImage = async () => {
+        const { data, error } = await supabase.storage.from('post-images').getPublicUrl('Screenshot (34).png')
+        console.log('error', error)
+        console.log('data', data)
+        return data.publicURL
+    }
+
+    return (
+        <img src={image} alt="screenshot" />
+    )
 }
 
 Create.propTypes = {}
